f5ed079b487921f67e4c0ae432e0ad1ddd84b56b
冰水不冰 <138358521+IceWaterNotIce@users.noreply.github.com>
2023-11-29 03:21:28 +0000

    Fix incorrect reference to canvas width in Shoot_icon instantiation

--- assets/js/Main.js
+++ assets/js/Main.js
@@ -75,7 +75,7 @@ window.GameArea = {
             var Shoot_icon_img = new Image();
             Shoot_icon_img.src = "{{ site.url }}{{ site.baseurl }}/assets/img\\ui\\Shoot_icon_1.png";
             Shoot_icon_img.onload = () => {
-                GameArea.Shoot_icon = new SpiritsLib.ImgComponent(GameArea.canvas.width / 2, GameArea.canvas.width / 2, GameArea.canvas.width - Shoot_icon_img.width, GameArea.canvas.height - Shoot_icon_img.height, 0, 0, Shoot_icon_img, 0.5);
+                GameArea.Shoot_icon = new SpiritsLib.ImgComponent(GameArea.canvas.width / 2, this.canvas.width / 2, GameArea.canvas.width - Shoot_icon_img.width, GameArea.canvas.height - Shoot_icon_img.height, 0, 0, Shoot_icon_img, 0.5);
             }
             PlayerInputLib.mobileInput();
         }
